386d6f27b762ec49555499813b2c69d83d3a0289
Theo Ferguson <ferguson.theo@gmail.com>
2021-08-09 13:40:42 -0400

    commit tree 6a282bd05541e975367b2a6694eeddc4d0d92e4e

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 62 / 2;
 
-// Write your code here
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+let random = Math.round(Math.random() * 100) + 1;
+
+let num3 = 54;
+
+let num4 = 5;
+
+let mod = num3 % num4;
+
+let max = Math.max( 1, 2, 3, 4, 5, 20);
